33c9812da189173c0c0441ca6aa877380150524f
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2014-08-05 23:05:15 +0530

    rebase and change API to support mpl colorspec

--- skimage/graph/__init__.py
+++ skimage/graph/__init__.py
@@ -4,6 +4,7 @@ from .rag import rag_mean_color, RAG
 from .graph_cut import cut_threshold, cut_normalized
 ncut = cut_normalized
 from .rag import rag_mean_color, RAG, rag_draw
+from .rag import rag_mean_color, RAG, draw_rag
 from .graph_cut import cut_threshold
 
 
@@ -16,9 +17,13 @@ __all__ = ['shortest_path',
            'rag_mean_color',
            'cut_threshold',
 <<<<<<< HEAD
+<<<<<<< HEAD
            'cut_normalized',
            'ncut',
 =======
            'rag_draw',
 >>>>>>> Docstrings
+=======
+           'draw_rag',
+>>>>>>> rebase and change API to support mpl colorspec
            'RAG']
